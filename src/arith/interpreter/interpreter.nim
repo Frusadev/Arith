077d7e43448b42fbc286fa16d4aa2f777c3936fb
Frusadev <frusadev@gmail.com>
2024-10-21 20:23:50 +0000

    Update interpreter structure

--- src/arith/interpreter/interpreter.nim
+++ src/arith/interpreter/interpreter.nim
@@ -1,26 +1,46 @@
 import strutils
 import arith/parser/parser
 import arith/lexer/lexer
+import arith/interpreter/runtime/scope
 
-proc interpret*(node: ASTNode): float =
+type
+   Interpreter = ref object
+      currentScope: Scope
+
+
+
+
+
+proc interpret*(interpreter: Interpreter, node: ASTNode): float =
    if node != nil:
       case node.nodeType
+      of COMPSTMT:
+         # create scope
+         let scope = initScope()
+         scope.parent = interpreter.currentScope
+      of ASSIGNSTMT:
+         discard
+      of VOIDSTMT:
+         discard
+      of VAR:
+         discard
       of BINOP:
          case node.token.tokType
          of PLUS:
-            return interpret(node.left) + interpret(node.right)
+            return interpreter.interpret(node.left) + interpreter.interpret(node.right)
          of MINUS:
-            return interpret(node.left) - interpret(node.right)
+            return interpreter.interpret(node.left) - interpreter.interpret(node.right)
          of MUL:
-            return interpret(node.left) * interpret(node.right)
+            return interpreter.interpret(node.left) * interpreter.interpret(node.right)
          of DIV:
-            return interpret(node.left) / interpret(node.right)
+            return interpreter.interpret(node.left) / interpreter.interpret(node.right)
          else:
             discard
       of NUM:
          return parseFloat(node.token.tokenValue)
       of UNOP:
+         let node = UnaryOperatorNode(node)
          if node.token.tokType == PLUS:
-            return +interpret(node.expr)
+            return +interpreter.interpret(node.expr)
          else:
-            return -interpret(node.expr)
+            return -interpreter.interpret(node.expr)
